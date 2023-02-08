9dd587a9903d9834bf827ae90acf03633bcb1d77
Ondeyo Matoke <ondeyostephen30@gmail.com>
2023-02-09 01:57:39 +0300

    changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ export default function Projects() {
         <a href='https://gift-card-delta.vercel.app/'>
           <div>
             <img src='/images/giftcard.png' alt='giftcardimage' className='giftcardimage'/>
-            <p className='heading5'>I'm working on this project with my two friends, a designer and backend dev.<br/>
+            <p className='heading5'>I'm working on this giftcard project with my two friends, a designer and backend dev.<br/>
               Once complete, users will be able to redeem their giftcards for cash.                
             </p>
           </div>
@@ -18,7 +18,7 @@ export default function Projects() {
         <a href='https://a-todo-list-gold.vercel.app/'>
           <div>
             <img src='/images/todo.png' alt='todolist' className='todoimage'/>
-            <p className='heading5'>If you like to have an organized day, then this a project you will 
+            <p className='heading5'>If you like to have an organized day, this toDo List project you will 
               be interested in. 
             </p>
           </div>
@@ -29,8 +29,8 @@ export default function Projects() {
         <a href='https://bmi-calculator-tan-three.vercel.app/'>
           <div>
             <img src='/images/bmi.png' alt='bmicalci' className='bmiimage'/>
-            <p className='heading5'>This calculator is for users on a weight management journey who would like to <br/> 
-              track their progress. Its very simple to use.
+            <p className='heading5'>This calculator project is for users on a weight management journey who would like<br/>
+             to track their progress. Its very simple to use.
             </p>
           </div>
         </a>
@@ -40,7 +40,7 @@ export default function Projects() {
         <a href='https://updated-clock.vercel.app/'>
           <div>
             <img src='/images/clock.png' alt='clock' className='clockimage'/>
-            <p className='heading5'>This was one of my very first projects. To help grasp JS projects.</p>
+            <p className='heading5'>This clock was one of my very first projects. To help grasp JS projects.</p>
           </div>
         </a>
       </div>
@@ -49,7 +49,7 @@ export default function Projects() {
         <a href='https://facebook-landing-page-sigma.vercel.app/'>
           <div>
             <img src='/images/facebook.png' alt='fbpage' className='fbimage'/>
-            <p className='heading5'>This is a facebook landing page. 
+            <p className='heading5'>This is a facebook landing page project. 
             I used it to practice HTML and CSS.</p>
           </div>
         </a>
