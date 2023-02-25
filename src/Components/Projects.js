eb7235f84482f810c78ea8dc91aae61af99ac358
Ondeyo Matoke <ondeyostephen30@gmail.com>
2023-02-25 23:22:30 +0300

    changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,7 +6,7 @@ export default function Projects() {
 
 
     <div className='project'>   
-        <a href='https://math-calculator-gnzv-git-master-ondeyomatoke.vercel.app/' target='blank'>
+        <a href='https://math-calculator-gnzv-git-master-ondeyomatoke.vercel.app/'>
           <div>
             <img src='/images/mathCalci.png' alt='mathCalci' className='mathCalciimage'/>
             <p className='heading5'> Making math calculations easy</p>
@@ -16,7 +16,7 @@ export default function Projects() {
 
 
       <div className='project'>
-        <a href='https://gift-card-git-master-ondeyomatoke.vercel.app/' target='blank'>
+        <a href='https://gift-card-git-master-ondeyomatoke.vercel.app/'>
           <div>
             <img src='/images/giftcard.png' alt='giftcardimage' className='giftcardimage'/>
             <p className='heading5'>I'm working on this giftcard project with my two friends, a designer and backend dev.<br/>
@@ -27,7 +27,7 @@ export default function Projects() {
       </div>
 
       <div className='project'>
-        <a href='https://a-todo-list-git-master-ondeyomatoke.vercel.app/' target='blank'>
+        <a href='https://a-todo-list-git-master-ondeyomatoke.vercel.app/'>
           <div>
             <img src='/images/todo.png' alt='todolist' className='todoimage'/>
             <p className='heading5'>If you like to have an organized day, this toDo List project you will 
@@ -38,7 +38,7 @@ export default function Projects() {
       </div>
 
       <div className='project'>
-        <a href='https://bmi-calculator-git-master-ondeyomatoke.vercel.app/' target='blank'>
+        <a href='https://bmi-calculator-git-master-ondeyomatoke.vercel.app/'>
           <div>
             <img src='/images/bmi.png' alt='bmicalci' className='bmiimage'/>
             <p className='heading5'>This calculator project is for users on a weight management journey who would like<br/>
@@ -49,7 +49,7 @@ export default function Projects() {
       </div>
 
       <div className='project'>
-        <a href='https://updated-clock-git-master-ondeyomatoke.vercel.app/' target='blank'>
+        <a href='https://updated-clock-git-master-ondeyomatoke.vercel.app/'>
           <div>
             <img src='/images/clock.png' alt='clock' className='clockimage'/>
             <p className='heading5'>This clock was one of my very first projects. To help grasp JS projects.</p>
@@ -58,7 +58,7 @@ export default function Projects() {
       </div>
 
       <div className='project'>
-        <a href='https://facebook-landing-page-git-master-ondeyomatoke.vercel.app/' target='blank'>
+        <a href='https://facebook-landing-page-git-master-ondeyomatoke.vercel.app/'>
           <div>
             <img src='/images/facebook.png' alt='fbpage' className='fbimage'/>
             <p className='heading5'>This is a facebook landing page project. 
